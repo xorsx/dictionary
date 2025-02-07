6f0466705c9b8249d0901315e56cc68fc512d516
xorsx <oliviarswann@gmail.com>
2025-02-07 16:19:30 -0500

    added synonym component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,14 +1,26 @@
 import React from "react";
+import Synonyms from "./Synonyms.js";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
-      <p>{props.meaning.definitions[0].definition}</p>
-      <p>
-        <em>{props.meaning.definitions[0].example}</em>
-      </p>
+      <h3>{props.meaning.partOfSpeech}</h3>{" "}
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              <strong>Definition: </strong>
+              {definition.definition}
+              <br />
+              <strong>Example: </strong>
+              <em>{definition.example}</em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
